a60cdeb16ed1b7be0552598c26c08761ae63f128
henryhu <826046954@qq.com>
2026-06-14 19:41:06 +0800

    feat: add Embedding field to DocumentChunk

--- model/document_chunk.go
+++ model/document_chunk.go
@@ -7,6 +7,7 @@ type DocumentChunk struct {
 	ID         uint      `gorm:"primaryKey;autoIncrement" json:"id"`
 	CourseID   uint      `gorm:"not null;index" json:"course_id"`
 	Content    string    `gorm:"type:text;not null" json:"content"`
+	Embedding  []float32 `gorm:"type:blob" json:"-"`
 	ChunkIndex int       `gorm:"not null;default:0" json:"chunk_index"`
 	CreatedAt  time.Time `gorm:"autoCreateTime" json:"created_at"`
 
